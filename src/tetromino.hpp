4e55e9e575c24a0d5cb2b39b673f5e84715e07e9
Jeramy Harrison <dbralir@gmail.com>
2018-08-12 02:16:09 -0500

    Added random colors

--- src/tetromino.hpp
+++ src/tetromino.hpp
@@ -4,6 +4,7 @@
 #include "components.hpp"
 
 #include <random>
+#include <algorithm>
 
 template <typename Rng>
 auto get_random_shape(Rng& rng) -> component::shape {
@@ -11,7 +12,7 @@ auto get_random_shape(Rng& rng) -> component::shape {
     const auto i_block = component::shape{{{{0, 0}, {0, 1}, {0, 2}, {0, 3}}}, {}, {0.5f, 1.5f}};
     const auto t_block = component::shape{{{{0, 0}, {1, 0}, {1, 1}, {2, 0}}}, {}, {1.f, 0.f}};
     const auto j_block = component::shape{{{{0, 0}, {1, 0}, {1, 1}, {1, 2}}}, {}, {1.f, 1.f}};
-    const auto l_block = component::shape{{{{0, 0}, {0, 1}, {0, 2}, {1, 0}}}, {}, {0.f, 1.f}};
+    const auto l_block = component::shape{{{{0, 2}, {0, 1}, {0, 0}, {1, 0}}}, {}, {0.f, 1.f}};
     const auto s_block = component::shape{{{{0, 0}, {1, 0}, {1, 1}, {2, 1}}}, {}, {1.f, 0.f}};
     const auto z_block = component::shape{{{{0, 1}, {1, 1}, {1, 0}, {2, 0}}}, {}, {1.f, 0.f}};
 
@@ -25,9 +26,15 @@ auto get_random_shape(Rng& rng) -> component::shape {
         z_block,
     };
 
-    auto roll = std::uniform_int_distribution{0, 6};
+    auto roll_block = std::uniform_int_distribution{0, 6};
+    auto roll_color = std::uniform_int_distribution{1, 2};
 
-    return blocks[roll(rng)];
+    auto block = blocks[roll_block(rng)];
+
+    block.colors[0] = roll_color(rng);
+    block.colors[3] = roll_color(rng);
+
+    return block;
 }
 
 #endif // LD42_TETROMINO_HPP
